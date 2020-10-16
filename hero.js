b4ca712e8bfdec5a01ea7cfa8ad0331be222653c
MelinaRentzi <r.melina_92@hotmail.com>
2020-10-16 12:01:36 +0300

    Added method getGenderRaceAndClass and created a second hero

--- hero.js
+++ hero.js
@@ -1,7 +1,10 @@
 class Hero {
-    constructor(name, life) {
+    constructor(name, life, gender, race, heroclass) {
         this.name = name;
         this.life = life;
+        this.gender = gender;
+        this.race = race;
+        this.heroclass = heroclass;
     }
     getBackStory() {
         console.log(`Hello, my name is ${this.name}.`);
@@ -10,8 +13,26 @@ class Hero {
     getHitPoints() {
         console.log(`${this.name} has ${this.life} HP.`);
     }
+
+    getGenderRaceAndClass() {
+        console.log(`${this.name} is a ${this.gender} ${this.race} ${this.heroclass}.`);
+    }
+
+    constructor(items) {
+        this.items = {
+            item1: sword,
+            item2: shield,
+            item3: grenade
+        }
+    }
 }
 
-const hero = new Hero('Melina', 20);
+const hero = new Hero('Melina', 20, 'Female', 'Human', 'Warrior');
 hero.getBackStory();
 hero.getHitPoints();
+hero.getGenderRaceAndClass();
+
+const hero2 = new Hero('Peanut', 30, 'Female', 'Asura', 'Engineer');
+hero2.getBackStory();
+hero2.getHitPoints();
+hero2.getGenderRaceAndClass();
